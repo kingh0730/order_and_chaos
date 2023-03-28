d7bb62cdea11a52e9eb6948806edff203d2b68c3
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-27 20:30:36 -0700

    interesting

--- position4x4.cu
+++ position4x4.cu
@@ -1,1 +1,16 @@
 #include "position4x4.h"
+
+#define ROW_OOOO 0b10101010
+#define ROW_XXXX 0b11111111
+
+bool Position4x4::has_4_in_a_row()
+{
+    if (this->row0 == ROW_OOOO)
+    {
+        return true;
+    }
+    if (this->row0 == ROW_XXXX)
+    {
+        return true;
+    }
+}
