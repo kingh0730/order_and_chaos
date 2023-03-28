fef4a72993421755a8c612e8cb5afb8aefbc27ea
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-27 21:35:42 -0700

    fine

--- player.cu
+++ player.cu
@@ -1,1 +1,14 @@
 #include "player.h"
+
+void print_player(Player player)
+{
+    switch (player)
+    {
+    case Player::Order:
+        std::cout << "Player::Order";
+    case Player::Chaos:
+        std::cout << "Player::Chaos";
+    default:
+        std::cout << "Player::Unknown";
+    }
+}
--- player.h
+++ player.h
@@ -8,15 +8,4 @@ enum Player
     Chaos,
 };
 
-void print_player(Player player)
-{
-    switch (player)
-    {
-    case Player::Order:
-        std::cout << "Player::Order";
-    case Player::Chaos:
-        std::cout << "Player::Chaos";
-    default:
-        std::cout << "Player::Unknown";
-    }
-}
+void print_player(Player player);
