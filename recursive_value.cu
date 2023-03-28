37b069b9a39d3751fafeccd18ebf3446fd9118b8
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-28 16:44:08 -0700

    cheng

--- recursive_value.cu
+++ recursive_value.cu
@@ -1,1 +1,25 @@
+#include <iostream>
+
 #include "recursive_value.h"
+
+GameResult to_game_result(const PrimitiveValue &pv)
+{
+    switch (pv)
+    {
+    case PrimitiveValue::NotPrimitive:
+        return GameResult::Undecided;
+
+    case PrimitiveValue::Win:
+        return GameResult::Win;
+
+    case PrimitiveValue::Lose:
+        return GameResult::Lose;
+
+    case PrimitiveValue::Tie:
+        return GameResult::Tie;
+
+    default:
+        std::cerr << "Unknown primitive value: " << pv << std::endl;
+        throw std::invalid_argument("Unknown primitive value");
+    }
+}
--- recursive_value.h
+++ recursive_value.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#include "primitive_value.h"
+
 enum GameResult
 {
+    Undecided,
     Win,
     Lose,
     Tie,
     Draw,
 };
+
+GameResult to_game_result(const PrimitiveValue &pv);
 
 // TODO
 class GameResultWithRmt
