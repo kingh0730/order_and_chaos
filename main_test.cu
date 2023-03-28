1c4debf91d5df12973d93f56e8da62a2b3867464
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-27 23:36:19 -0700

    good

---
+++ main_test.cu
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+
+#include "position4x4.h"
+#include "move.h"
+
+int main()
+{
+    std::cout << "GamesCrafters!\n"
+              << std::endl;
+
+    Position4x4 p = Position4x4();
+    std::cout << p.format();
+
+    Move m = Move(Move::Piece::X, 0, 0);
+    std::cout << m.format() << '\n';
+
+    // Generate moves
+    auto moves = p.generate_moves();
+    auto moves_size = moves.size();
+
+    assert(moves_size == 32);
+    std::cout << moves_size << '\n';
+
+    for (Move m : moves)
+    {
+        std::cout << m.format() << '\n';
+    }
+}
