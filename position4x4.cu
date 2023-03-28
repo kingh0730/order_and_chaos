14cf5e7680f5f4db019cb9d1f4391d80ac3b3916
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-27 21:43:30 -0700

    interseting

--- position4x4.cu
+++ position4x4.cu
@@ -1,9 +1,14 @@
+#include <iostream>
+#include <bitset>
+
 #include "position4x4.h"
 
 #define ROW_OOOO 0b10101010
 #define ROW_XXXX 0b11111111
 
-Position4x4::Position4x4(Player player) : player(player), rows({0, 0, 0, 0}) {}
+Position4x4::Position4x4() : Position4x4(Player::Order) {}
+Position4x4::Position4x4(Player player) : player(player), rows{0, 0, 0, 0} {}
+Position4x4::Position4x4(Player player, char rows[4]) : player(player), rows{*rows} {}
 
 bool Position4x4::has_4_in_a_row()
 {
@@ -17,5 +22,20 @@ bool Position4x4::has_4_in_a_row()
         {
             return true;
         }
+    }
+
+    // TODO
+
+    return false;
+}
+
+void Position4x4::print()
+{
+    print_player(this->player);
+    std::cout << '\n';
+
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << std::bitset<8>(this->rows[i]) << '\n';
     }
 }
