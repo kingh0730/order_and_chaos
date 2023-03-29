42a235d518471c6acc929f7868e818d3a571efeb
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-29 04:05:52 -0700

    very interesting

--- src/main.cu
+++ src/main.cu
@@ -54,6 +54,32 @@ Move get_input()
     return Move(input_piece, input_i, input_j);
 }
 
+void one_game()
+{
+    Position4x4 p = Position4x4();
+
+    while (true)
+    {
+        std::cout << p.format_pretty() << '\n';
+
+        // Primitive value
+        PrimitiveValue pv = p.primitive_value();
+        if (pv != PrimitiveValue::NotPrimitive)
+        {
+            std::cout << format_primitive_value(pv) << '\n';
+            break;
+        }
+
+        // Inputs
+        Move input_move = get_input();
+        std::cout << "Chosen move: " << input_move.format() << '\n'
+                  << std::endl;
+
+        // Do move
+        p = p.do_move(input_move);
+    }
+}
+
 int main()
 {
     std::cout << "GamesCrafters!\n"
@@ -65,14 +91,6 @@ int main()
 
     // std::cout << format_game_result(gr) << '\n';
 
-    while (true)
-    {
-        std::cout << p.format_pretty() << '\n';
-
-        // Inputs
-        Move input_move = get_input();
-        std::cout << "Chosen move: " << input_move.format() << '\n';
-
-        break;
-    }
+    // TODO game loop
+    one_game();
 }
