14c35244483d8fc80be0a8fc526bdc820b5417a6
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-29 00:42:08 -0700

    ok

--- src/lib/solver.h
+++ src/lib/solver.h
@@ -27,10 +27,19 @@ class Solver
 private:
     std::map<Position4x4, GameResult> memoized_game_results;
 
-    GameResult memoized_to_game_result(
-        const Position4x4 &cur_position, const MemoizedGameResult &mgr);
+    std::map<Position4x4, MemoizedState1> memoized_states1;
+    std::map<Position4x4, MemoizedState2> memoized_states2;
 
-    MemoizedGameResult game_result_to_memoized(
+    // TODO memoized states
+    GameResult memoized_states_to_game_result(
+        const Position4x4 &cur_position,
+        const MemoizedState1 &ms1,
+        const MemoizedState2 &ms2);
+
+    MemoizedState1 game_result_to_memoized_state1(
+        const Position4x4 &cur_position, const GameResult &gr);
+
+    MemoizedState2 game_result_to_memoized_state2(
         const Position4x4 &cur_position, const GameResult &gr);
 
 private:
@@ -41,7 +50,7 @@ private:
         const Position4x4 &position, const Move &move);
 
 public:
-    Solver() : memoized(std::map<Position4x4, MemoizedGameResult>()) {}
+    Solver() : memoized_game_results(std::map<Position4x4, GameResult>()) {}
 
     GameResult solve_not_memoized(const Position4x4 &position);
     GameResult solve(const Position4x4 &position);
