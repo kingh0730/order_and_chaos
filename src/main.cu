ae9a4bc5724b0facf0ed2e0be7238f6addc46514
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-29 03:30:36 -0700

    ok

--- src/main.cu
+++ src/main.cu
@@ -10,7 +10,20 @@ int main()
 
     Position4x4 p = Position4x4();
     Solver solver = Solver();
-    GameResult gr = solver.solve(p);
+    // GameResult gr = solver.solve(p);
 
-    std::cout << format_game_result(gr) << '\n';
+    // std::cout << format_game_result(gr) << '\n';
+
+    while (true)
+    {
+        std::cout << p.format_pretty() << '\n';
+
+        int input_i, input_j;
+        std::cout << "Input i: ";
+        std::cin >> input_i;
+        std::cout << "Input j: ";
+        std::cin >> input_j;
+
+        break;
+    }
 }
