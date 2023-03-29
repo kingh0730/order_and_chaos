9a1ef23b24ad4c8bf211b1187f0f8b6cb5ab7b73
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-29 03:56:29 -0700

    could

--- src/main.cu
+++ src/main.cu
@@ -3,6 +3,57 @@
 #include "position4x4.h"
 #include "solver.h"
 
+Move get_input()
+{
+    size_t input_i;
+    size_t input_j;
+    Move::Piece input_piece;
+    std::string input_piece_str;
+
+    while (true)
+    {
+        input_i = 0;
+        input_j = 0;
+        input_piece_str = "";
+
+        std::cout << "Input move...\n";
+
+        std::cout << "Input i: ";
+        std::cin >> input_i;
+        if (input_i >= 4)
+        {
+            continue;
+        }
+
+        std::cout << "Input j: ";
+        std::cin >> input_j;
+        if (input_j >= 4)
+        {
+            continue;
+        }
+
+        std::cout << "Input piece: ";
+        std::cin >> input_piece_str;
+
+        if (input_piece_str == "o" || input_piece_str == "O")
+        {
+            input_piece = Move::Piece::O;
+        }
+        else if (input_piece_str == "x" || input_piece_str == "X")
+        {
+            input_piece = Move::Piece::X;
+        }
+        else
+        {
+            continue;
+        }
+
+        break;
+    }
+
+    return Move(input_piece, input_i, input_j);
+}
+
 int main()
 {
     std::cout << "GamesCrafters!\n"
@@ -18,11 +69,9 @@ int main()
     {
         std::cout << p.format_pretty() << '\n';
 
-        int input_i, input_j;
-        std::cout << "Input i: ";
-        std::cin >> input_i;
-        std::cout << "Input j: ";
-        std::cin >> input_j;
+        // Inputs
+        Move input_move = get_input();
+        std::cout << "Chosen move: " << input_move.format() << '\n';
 
         break;
     }
