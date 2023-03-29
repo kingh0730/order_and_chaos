6b60658dd81ad4c23977cfa5317ad9095f86b5ed
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-29 00:09:42 -0700

    good

--- src/lib/solver.h
+++ src/lib/solver.h
@@ -8,8 +8,21 @@
 
 class Solver
 {
+    // Keep small as map value
+    enum MemoizedGameResult : char
+    {
+        OrderLoses,
+        OrderWins,
+    };
+
 private:
-    std::map<Position4x4, GameResult> memoized;
+    std::map<Position4x4, MemoizedGameResult> memoized;
+
+    GameResult memoized_to_game_result(
+        const Position4x4 &cur_position, const MemoizedGameResult &mgr);
+
+    MemoizedGameResult game_result_to_memoized(
+        const Position4x4 &cur_position, const GameResult &gr);
 
 private:
     std::vector<GameResult> solve_children(
@@ -19,7 +32,7 @@ private:
         const Position4x4 &position, const Move &move);
 
 public:
-    Solver() : memoized(std::map<Position4x4, GameResult>()) {}
+    Solver() : memoized(std::map<Position4x4, MemoizedGameResult>()) {}
 
     GameResult solve_not_memoized(const Position4x4 &position);
     GameResult solve(const Position4x4 &position);
