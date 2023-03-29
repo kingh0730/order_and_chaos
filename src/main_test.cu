dc2a6710a9b819dc22945beaedef4866f25bdcf3
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-28 23:44:54 -0700

    good

--- src/main_test.cu
+++ src/main_test.cu
@@ -10,6 +10,9 @@ int main()
     std::cout << "GamesCrafters!\n"
               << std::endl;
 
+    // GameResult
+    std::cout << "sizeof(GameResult): " << sizeof(GameResult) << '\n';
+
     // Positions
     Position4x4 p = Position4x4();
     std::cout << p.format();
