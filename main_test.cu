635e2d942c96cb0931058cda692fa4852326a74c
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-03-28 18:41:09 -0700

    tests

--- main_test.cu
+++ main_test.cu
@@ -10,10 +10,44 @@ int main()
     std::cout << "GamesCrafters!\n"
               << std::endl;
 
+    // Positions
     Position4x4 p = Position4x4();
     std::cout << p.format();
     std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::NotPrimitive);
 
+    char all_4[4] = {0b10101010, 0b00000000, 0b00000000, 0b00000000};
+    char empty[4] = {0b00000000, 0b00000000, 0b00000000, 0b00000000};
+    char pos_diag = 0b10101010;
+    char neg_diag = 0b11111111;
+    int num_spaces_remain = 16;
+
+    p = Position4x4(Player::Order, all_4, empty, 0, 0, 12);
+    std::cout << p.format();
+    std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::Win);
+
+    p = Position4x4(Player::Chaos, empty, all_4, 0, 0, 12);
+    std::cout << p.format();
+    std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::Lose);
+
+    p = Position4x4(Player::Order, empty, empty, pos_diag, 0, 12);
+    std::cout << p.format();
+    std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::Win);
+
+    p = Position4x4(Player::Chaos, empty, empty, 0, neg_diag, 12);
+    std::cout << p.format();
+    std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::Lose);
+
+    p = Position4x4(Player::Order, empty, empty, 0, 0, 0);
+    std::cout << p.format();
+    std::cout << p.format_pretty();
+    assert(p.primitive_value() == PrimitiveValue::Lose);
+
+    // Moves
     Move m = Move(Move::Piece::X, 0, 0);
     std::cout << m.format() << '\n';
 
