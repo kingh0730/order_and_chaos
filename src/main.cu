91ff739e20242fb630e5fffbefe7f0d15897d48e
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 13:01:42 -0800

    works

--- src/main.cu
+++ src/main.cu
@@ -1,10 +1,13 @@
-#include "_lib.cu"
+#include "_player.h"
 #include <iostream>
 
 #define N 3
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
-  std::cout << hi() << std::endl;
+
+  const Player p1 = Player::X;
+  std::cout << format_player(p1) << std::endl;
+
   return 0;
 }
