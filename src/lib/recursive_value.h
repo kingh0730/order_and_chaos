cf3d9b769291bbde6d87f796f63c309e42e36060
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 15:36:32 -0800

    interesting

--- src/lib/recursive_value.h
+++ src/lib/recursive_value.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <string>
+
+class RecursiveValue {
+public:
+  enum _RecursiveValueType {
+    Undecided = 0,
+    Win = 1,
+    Lose = 2,
+    Tie = 3,
+    Draw = 4,
+  };
+
+private:
+  _RecursiveValueType rv;
+
+public:
+  RecursiveValue(_RecursiveValueType rv) : rv(rv) {}
+
+  bool operator==(const RecursiveValue &other) const { return rv == other.rv; }
+
+  std::string format() const;
+};
