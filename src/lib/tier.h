9fea94ad38bce67a09649a7f1087d1b4194a6d46
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 15:39:46 -0800

    works

--- src/lib/tier.h
+++ src/lib/tier.h
@@ -1,15 +1,16 @@
 #pragma once
 
 #include "position.h"
+#include "recursive_value.h"
 #include <map>
 
 class Tier {
 private:
   Tier *next_tier;
-  std::map<Position, PrimitiveValue> position_to_pv;
+  std::map<Position, RecursiveValue> position_to_rv;
 
 public:
   Tier(int num_empty_spaces, Tier *next_tier) : next_tier(next_tier) {
-    position_to_pv = std::map<Position, PrimitiveValue>();
+    position_to_rv = std::map<Position, RecursiveValue>();
   }
 };
