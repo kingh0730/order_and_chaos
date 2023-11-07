52b41d9d7316f1e5208b6297bc1815643e8ad86a
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 15:03:01 -0800

    correct

--- src/lib/position.cu
+++ src/lib/position.cu
@@ -1,4 +1,40 @@
 #include "position.h"
+#include <iostream>
+
+Player::PlayerType Position::piece_to_player(Board::PieceType piece) {
+  switch (piece) {
+  case Board::X:
+    return Player::X;
+  case Board::O:
+    return Player::O;
+  default:
+    throw std::invalid_argument("Invalid piece type");
+  }
+}
+
+Board::PieceType Position::player_to_piece(Player::PlayerType player) {
+  switch (player) {
+  case Player::X:
+    return Board::X;
+  case Player::O:
+    return Board::O;
+  default:
+    throw std::invalid_argument("Invalid player");
+  }
+}
+
+PrimitiveValue Position::primitive_value() const {
+  Player::PlayerType opponent_type = player.next_player_type();
+  Board::PieceType opponent_piece = player_to_piece(opponent_type);
+
+  if (board.is_win_for(opponent_piece)) {
+    return PrimitiveValue::Lose;
+  } else if (board.is_full()) {
+    return PrimitiveValue::Tie;
+  } else {
+    return PrimitiveValue::NotPrimitive;
+  }
+}
 
 std::string Position::format() const {
   auto player_str = player.format();
