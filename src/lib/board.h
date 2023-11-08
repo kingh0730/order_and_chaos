c221ca05440661c77aac6618645a3d3c2699b07f
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-08 00:43:38 -0800

    fix

--- src/lib/board.h
+++ src/lib/board.h
@@ -18,8 +18,8 @@ private:
   PieceType board[TTT_N][TTT_N];
 
 private:
-  static std::vector<std::map<std::string, unsigned long long>>
-      id_str_to_id_for_empty_spaces;
+  static std::vector<std::map<unsigned long long, unsigned long long>>
+      id_raw_to_id_for_empty_spaces;
 
 public:
   Board(PieceType board[TTT_N][TTT_N]);
@@ -27,7 +27,7 @@ public:
   bool is_win_for(PieceType piece) const;
   bool is_full() const;
 
-  std::string id_str_for_empty_spaces() const;
+  unsigned long long id_raw_for_empty_spaces() const;
   unsigned long long id() const;
   static unsigned long long num_boards(unsigned int num_empty_spaces);
 
