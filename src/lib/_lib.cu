bd143a3bc6734bde89184ebcddeb62b27b9931c7
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 23:38:26 -0800

    del

--- src/lib/_lib.cu
+++
@@ -1,9 +0,0 @@
-__global__ void vectorAdd(int *a, int *b, int *c, int N) {
-  // Calculate global thread thread ID
-  int tid = (blockDim.x * blockIdx.x) + threadIdx.x;
-
-  // Boundary check
-  if (tid < N) {
-    c[tid] = a[tid] + b[tid];
-  }
-}
