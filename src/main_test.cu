62c9348c7af6745682ab27c7a8f1b6136a8e4867
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 23:24:49 -0800

    something

--- src/main_test.cu
+++ src/main_test.cu
@@ -31,6 +31,12 @@ void test_utils() {
   assert(factorial(1) == 1);
   assert(factorial(2) == 2);
   assert(factorial(3) == 6);
+  assert(factorial(4) == 24);
+  assert(factorial(5) == 120);
+  assert(factorial(6) == 720);
+  assert(factorial(7) == 5040);
+  assert(factorial(8) == 40320);
+  assert(factorial(9) == 362880);
 }
 
 void test_boards() {
