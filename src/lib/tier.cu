99a786d905d905e14c4f8b42f34bafc8592331aa
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 21:43:03 -0800

    interesting

--- src/lib/tier.cu
+++ src/lib/tier.cu
@@ -1,5 +1,16 @@
 #include "tier.h"
 
+Tier::SolveResult Tier::solve(SolveBy solve_by) {
+  switch (solve_by) {
+  case SolveBy::CPU:
+    solve_by_cpu(position_hash_to_rv, next_tier->position_hash_to_rv);
+    break;
+  case SolveBy::GPU:
+    solve_by_gpu(position_hash_to_rv, next_tier->position_hash_to_rv);
+    break;
+  }
+}
+
 void solve_by_cpu(RecursiveValue *position_hash_to_rv,
                   RecursiveValue *child_position_hash_to_rv) {
   position_hash_to_rv[0] = RecursiveValue::Tie;
