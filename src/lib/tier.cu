d02406efa09b66a4dba0ea41b5242fa5dc059454
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-07 21:41:27 -0800

    mock

--- src/lib/tier.cu
+++ src/lib/tier.cu
@@ -1,1 +1,11 @@
 #include "tier.h"
+
+void solve_by_cpu(RecursiveValue *position_hash_to_rv,
+                  RecursiveValue *child_position_hash_to_rv) {
+  position_hash_to_rv[0] = RecursiveValue::Tie;
+}
+
+__global__ void solve_by_gpu(RecursiveValue *position_hash_to_rv,
+                             RecursiveValue *child_position_hash_to_rv) {
+  position_hash_to_rv[0] = RecursiveValue::Tie;
+}
