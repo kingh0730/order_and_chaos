880034b173666a3e8057e2d998a968c1100bdda5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-10 23:46:02 -0800

    cout

--- src/lib/tier.cu
+++ src/lib/tier.cu
@@ -1,4 +1,5 @@
 #include "tier.h"
+#include <iostream>
 #include <sstream>
 #include <stdint.h>
 
@@ -14,12 +15,18 @@ Tier::Tier(unsigned int num_empty_spaces, Tier *next_tier)
 
 bool Tier::operator==(const Tier &other) const {
   if (num_empty_spaces != other.num_empty_spaces) {
+    std::cout << "num_empty_spaces: " << num_empty_spaces;
+    std::cout << " != " << other.num_empty_spaces << std::endl;
     return false;
   }
   if (num_positions != other.num_positions) {
+    std::cout << "num_positions: " << num_positions;
+    std::cout << " != " << other.num_positions << std::endl;
     return false;
   }
   if (solved != other.solved) {
+    std::cout << "solved: " << solved;
+    std::cout << " != " << other.solved << std::endl;
     return false;
   }
   // if (next_tier != other.next_tier) {
@@ -28,6 +35,9 @@ bool Tier::operator==(const Tier &other) const {
 
   for (unsigned long long i = 0; i < num_positions; i++) {
     if (position_hash_to_rv[i] != other.position_hash_to_rv[i]) {
+      std::cout << "position_hash_to_rv[" << i << "]: ";
+      std::cout << position_hash_to_rv[i].format();
+      std::cout << " != " << other.position_hash_to_rv[i].format() << std::endl;
       return false;
     }
   }
