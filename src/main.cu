c33cd2f0e41919ce2ee80f9e9d4b2083916af5ab
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-11 00:38:29 -0800

    i wanna play

--- src/main.cu
+++ src/main.cu
@@ -7,7 +7,7 @@ using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
 
-Tier *solve(Tier::SolveBy solve_by) {
+Tier *solve(Tier::SolveBy solve_by, bool destroy_tiers) {
   Tier *next_tier = nullptr;
 
   for (int i = 0; i < TTT_N * TTT_N + 1; i++) {
@@ -18,7 +18,7 @@ Tier *solve(Tier::SolveBy solve_by) {
       throw std::runtime_error("Failed to solve tier " + std::to_string(i));
     }
 
-    if (next_tier != nullptr) {
+    if (destroy_tiers && (next_tier != nullptr)) {
       delete next_tier;
       tier->set_next_tier_null();
     }
@@ -33,14 +33,14 @@ int main() {
   std::cout << "== Start!" << std::endl;
 
   auto t1 = high_resolution_clock::now();
-  auto tier_solved_by_cpu = solve(Tier::SolveBy::CPU);
+  auto tier_solved_by_cpu = solve(Tier::SolveBy::CPU, false);
   auto t2 = high_resolution_clock::now();
   std::cout << "CPU: ";
   std::cout << duration_cast<milliseconds>(t2 - t1).count() << "ms"
             << std::endl;
 
   auto t3 = high_resolution_clock::now();
-  auto tier_solved_by_gpu = solve(Tier::SolveBy::GPU);
+  auto tier_solved_by_gpu = solve(Tier::SolveBy::GPU, false);
   auto t4 = high_resolution_clock::now();
   std::cout << "GPU: ";
   std::cout << duration_cast<milliseconds>(t4 - t3).count() << "ms"
