3b56ffa1733bd50f6fb8c8476f80049c03d8de5d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-22 01:39:36 -0800

    fine

--- src/lib/player.h
+++ src/lib/player.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "utils.h"
 #include <string>
 
 class Player {
@@ -13,10 +14,10 @@ private:
   PlayerType player_type;
 
 public:
-  Player(unsigned int num_empty_spaces);
-  Player(PlayerType player_type) : player_type(player_type) {}
-  PlayerType get_player_type() const { return player_type; }
+  CUDA_CALLABLE Player(unsigned int num_empty_spaces);
+  CUDA_CALLABLE Player(PlayerType player_type) : player_type(player_type) {}
 
+  PlayerType get_player_type() const { return player_type; }
   PlayerType next_player_type() const;
 
   std::string format() const;
