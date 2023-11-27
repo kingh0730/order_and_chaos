df241a2d19ad2830749a11359519f12271e5d245
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-11-26 20:26:22 -0800

    nit

--- src/lib/tier.cu
+++ src/lib/tier.cu
@@ -7,6 +7,7 @@
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 using std::chrono::milliseconds;
+using std::chrono::steady_clock;
 
 Tier::Tier(unsigned int num_empty_spaces, Tier *next_tier)
     : num_empty_spaces(num_empty_spaces), next_tier(next_tier) {
@@ -81,9 +82,12 @@ Tier::SolveResult Tier::solve(SolveBy solve_by) {
     unsigned long long child_position_hash_to_rv_size =
         sizeof(RecursiveValue) * child_num_positions;
 
-    auto t1 = high_resolution_clock::now();
+    steady_clock::time_point t1;
+    steady_clock::time_point t2;
+
+    t1 = high_resolution_clock::now();
     cudaMalloc(&d_position_hash_to_rv, position_hash_to_rv_size);
-    auto t2 = high_resolution_clock::now();
+    t2 = high_resolution_clock::now();
     std::cout << "\tcudaMalloc(..., position_hash_to_rv_size): ";
     std::cout << duration_cast<milliseconds>(t2 - t1).count() << "ms"
               << " from time " << t1.time_since_epoch().count() << " to "
